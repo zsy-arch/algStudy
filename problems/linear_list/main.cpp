a637ce5db01c15ff575cebb2d32655a3c339de68
zsy-arch <helloworld1915@outlook.com>
2020-12-25 19:14:16 +0800

    linear list

--- problems/linear_list/main.cpp
+++ problems/linear_list/main.cpp
@@ -52,6 +52,37 @@ void insert(LinearList &L, int i, int value)
     p->next = tmp;
 }
 
+void insert_before(LinearList &L, LNode *origin, LNode *newNode)
+{
+    newNode->next = origin->next;
+    origin->next = newNode;
+    int v = origin->value;
+    origin->value = newNode->value;
+    newNode->value = v;
+}
+
+void insert_after(LinearList &L, LNode *origin, LNode *newNode)
+{
+    newNode->next = origin->next;
+    origin->next = newNode;
+}
+
+void erase(LinearList &L, int index, int *retVal = nullptr)
+{
+    int j = -1;
+    LNode *p = L;
+    while (j < index - 1)
+    {
+        j++;
+        p = p->next;
+    }
+    if (retVal != nullptr)
+    {
+        *retVal = p->next->value;
+    }
+    p->next = p->next->next;
+}
+
 int size(LinearList &L)
 {
     LNode *p = L;
@@ -64,6 +95,16 @@ int size(LinearList &L)
     return n;
 }
 
+void print(LinearList &L)
+{
+    LNode *p = L->next;
+    for (int i = 0; i < size(L); i++)
+    {
+        std::cout << p->value << std::endl;
+        p = p->next;
+    }
+}
+
 int main(int argc, const char *argv[])
 {
     LinearList list = new LNode;
@@ -74,8 +115,11 @@ int main(int argc, const char *argv[])
     insert(list, 2, 300);
     insert(list, 1, 400);
 
-    std::cout << index_of(list, 1)->value << '\n';
-
-    std::cout << size(list) << '\n';
+    LNode *node0 = index_of(list, 0);
+    LNode *newNode0 = new LNode;
+    newNode0->value = 1000;
+    insert_before(list, node0, newNode0);
+    erase(list, 1);
+    print(list);
     return 0;
 }
